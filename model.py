9ec320fad8a269cb462585f71201c756f5b77fe6
Ben Salem Meryem <124097610+MeryemBenSalem@users.noreply.github.com>
2025-01-26 21:12:30 +0100

    Update model.py

--- model.py
+++ model.py
@@ -16,10 +16,9 @@ from matplotlib import pyplot as plt
 df['label'].plot(kind='hist', bins=20, title='label')
 plt.gca().spines[['top', 'right',]].set_visible(False)
 
-# train test split
 train, test = train_test_split(df, test_size = 0.2, stratify = df['label'], random_state=3)
 
-# get the shape of train and test split.
+
 train.shape, test.shape
 
 # create a TF-IDF vectorizer object
@@ -28,20 +27,20 @@ tfidf_vectorizer = TfidfVectorizer(lowercase= True, max_features=1000, stop_word
 # fit the object with the training data tweets
 tfidf_vectorizer.fit(train.tweet)
 
-# transform the train and test data
+
 train_idf = tfidf_vectorizer.transform(train.tweet)
 test_idf  = tfidf_vectorizer.transform(test.tweet)
 
-# create the object of LinearRegression Model
+
 model_LR = LogisticRegression()
 
-# fit the model with the training data
+
 model_LR.fit(train_idf, train.label)
 
-# predict the label on the traning data
+
 predict_train = model_LR.predict(train_idf)
 
-# predict the model on the test data
+
 predict_test = model_LR.predict(test_idf)
 
 # define the stages of the pipeline
@@ -50,11 +49,11 @@ pipeline = Pipeline(steps= [('tfidf', TfidfVectorizer(lowercase=True,
                                                       stop_words= 'english')),
                             ('model', LogisticRegression())])
 
-# fit the pipeline model with the training data
+
 pipeline.fit(train.tweet, train.label)
 
 # sample tweet
 text = ["Excited to announce our latest product launch! Stay tuned for updates and get ready to elevate your experience. #productlaunch #innovation #excited"]
 
-# predict the label using the pipeline
+
 pipeline.predict(text)
